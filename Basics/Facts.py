e7c4e9e0512c161515e9ceeb533dd0357f727838
vikram <vikramdev772@gmail.com>
2023-11-08 13:50:27 +0530

    Python Basics

---
+++ Basics/Facts.py
@@ -0,0 +1,32 @@
+print("\n\t Amazing Facts \n")
+print("\n\t Ranked #1 (TIOBE) \n")
+
+# Highest rise in rating 2007,2010,2018,2020
+# it reduces app development time by 1/6
+# large organisations that uses Python are 
+
+print('\n\t Google,Netflix,Dropbox,Youtube,Instagram,Mozilla,Amazon, CERN ,NASA ....\n')
+
+# Where python cna be used ?
+
+# *Developing websites
+# *Task automation
+# *Data Analysis 
+# *Data visualization
+# *AI ,ML , IOT 
+# *Developing desktop applications.
+
+# Python can also be used in developing mobile applications, client side of AJAX based applications
+
+
+print("\n\t Python contains compiler but not similar to c/c++ \n")
+print("\n\t  .PYC : python specific Byte Code \n")
+
+print("\n\t Python Virtual Machine PVM \n")
+print('\n\t Windows : PVM 1 ')
+print('\n\t Linux : PVM 2 ')
+print('\n\t Mac : PVM 3 ')
+
+
+
+
---
+++ Basics/Main.py
@@ -0,0 +1,28 @@
+print("\n\t Hello world ! \n")
+
+# Python was conceired in the late 1980's by guido van rossum at centrum wiskunde & informatica in the
+# Netherlands as a Succesor of the ABC language.
+
+# He has been invariably worked as the lead developer, until 12 th July 2018
+
+# In Jan 2019 , active Python core developers elected a fine member council to lead project .
+
+print("\n\t Version History \n\t Python 1.0    1994,\n\t Python 2.0    2000")
+print("\t Python 3.0    2008 \n");
+
+print("\n\t Python developer aim for it \n\t to be fun to use .\n\t The name 'Python' is a tribute to the British comedy group \n\tThe Complete MONTHY PYTHON s flying  circus\n")
+
+print("\n\t Why you should learn Python ? ")
+print('\n\t* Huge Community Support ')
+print('\n\t* Future is with AI ')
+print('\n\t Easy to Learn and implement \n')
+print('\n\t General purpose programming language \n')
+
+print("\n\t Indentation base block of code \n")
+
+print("\n\t Large Library \n")
+print('\n\t Platform independent \n')
+
+
+
+
