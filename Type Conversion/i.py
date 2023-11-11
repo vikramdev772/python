45c43e518ccc2073e9a2281e81cdd6c5814a28b9
vikram <vikramdev772@gmail.com>
2023-11-11 17:56:06 +0530

    as keyword

---
+++ Type Conversion/i.py
@@ -0,0 +1,11 @@
+from e import x as p  # Assuming 'e' is a module or variable defined elsewhere
+x = 10
+y = 20  # Assuming y is defined somewhere
+
+print(p)
+print(y)
+
+# Hello world
+# 20
+
+""" so we as keyword to get the x value from e import x as P"""
