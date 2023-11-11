3f658a5cc025331432e412024aeaf81bdf96a969
vikram <vikramdev772@gmail.com>
2023-11-11 17:48:12 +0530

    Keywords

---
+++ Keywords/A1.py
@@ -0,0 +1,11 @@
+import keyword
+print(keyword.kwlist)
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 
+# 'await', 'break', 'class', 'continue', 'def', 'del', 'elif',
+#  'else', 'except', 'finally', 'for', 'from', 'global', 'if', 
+# 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 
+# 'return', 'try', 'while', 'with', 'yield']
+
+print("\n\tpyton is case sensitive language : False,x=None,p=True look like data \n")
+
+print("\n\t help() is a function \n")
---
+++ Keywords/Main.py
@@ -0,0 +1,20 @@
+
+ """
+    keywords
+
+    predefined words 
+    Reserved words
+
+    False   class   from    or
+    None    continue global pass
+    True    def     if      raise   
+    and     del     import  return
+    as      elif    in      try
+    assert  else    is      while
+    async   except  lamda   with    
+    await   finally non local  yield    
+    break   for     not
+
+ 
+ 
+ """
