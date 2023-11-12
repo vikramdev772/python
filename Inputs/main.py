ba494bdf8b17db7aedcad240e520e7b15ee0c84a
vikram <vikramdev772@gmail.com>
2023-11-12 08:58:12 +0530

    input

---
+++ Inputs/main.py
@@ -0,0 +1,48 @@
+
+""" Taking Input from User 
+
+    input()
+    .input() can take at most one argument of str type 
+    .input() always return str type value 
+
+
+"""
+print("\n\t Enter a value : ")
+x=input()
+print("\n\t X value : ",x)
+print(type(x))
+# <class 'str'>
+
+y=input("Enter your name : ")
+print(y)
+
+n=input("\n\t Enter a num : ")
+print(n)
+print("\n\t Type of n : ",type(n))
+#  Type of n :  <class 'str'>
+
+
+
+# v=int(input())
+
+# int() = int function as arguments int( args )  args = input()
+
+
+p=int(input("\n\t Enter a first num : "))
+q=int(input("\n\t Enter a secon num : "))
+r=p+q
+print("\n\t Result : ",r)
+
+#  Enter a first num : 2
+
+#          Enter a secon num : 3
+
+#          Result :  5
+
+
+
+
+
+
+
+
