b74259bfbe9c87a60face1d3531334ffb1c8404e
vikram <vikramdev772@gmail.com>
2023-11-12 08:46:37 +0530

    ORD

--- Type Conversion/Ns.py
+++ Type Conversion/Ns.py
@@ -17,3 +17,13 @@ Hexadecimal Number System -    0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
 
 
 """
+
+x=25
+print(bin(x))
+
+# 0b11001
+
+# character encoding 
+# ascii = Amercian standard code for information interchamge
+
+
---
+++ Type Conversion/Unicode/main.py
@@ -0,0 +1,43 @@
+"""
+        
+            Unicode 
+
+    The unicode  is character encoding , and its goal is to replace 
+    the existing character sets with its standard UTF.
+
+    UTF =  unicode Transformation Format
+
+    UTF-8 = it the most commonly used character encoding.
+    it is also backward compatible with ASCII
+
+        
+        """
+
+# ASCII code of Character A = 65
+
+# Conversion function
+
+
+
+
+
+# Character to unicode 
+print("Character to unicode ")
+x='A'
+print(ord(x))
+
+# The ord() function in Python stands for "ordinal." It is a built-in function that returns an integer representing the Unicode character. The name "ordinal" suggests that it returns the order or position of a character in the Unicode character set.
+
+print("Unicode to Character")
+y=97
+print(chr(y))
+# Unicode to Character
+# a
+# ord('ABC') //error  only single code 
+
+print(ord('b'))  #98
+
+print("\n\t ord means : operational readyness demonstration ")
+
+
+
