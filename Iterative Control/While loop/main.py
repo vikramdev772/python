b015960b9ad08894a20f03dda79a312be4594877
vikram <vikramdev772@gmail.com>
2023-11-13 09:02:44 +0530

    While

---
+++ Iterative Control/While loop/main.py
@@ -0,0 +1,8 @@
+
+print("Vikram")
+i=0
+while i<5:
+    print("\t Vikram ",end=" ") 
+    i=i+1
+
+print("\n\t The loop ends \n")
---
+++ Iterative Control/While loop/natural.py
@@ -0,0 +1,11 @@
+
+i=1;
+while i<=10:
+    print("\t ",i,end=" ")
+    i+=1
+
+print("\n The loop ends \n")
+#  1 	  2 	  3 	  4 	  5 	  6 	  7 	  8 	  9 	  10 
+#  The loop ends 
+
+
