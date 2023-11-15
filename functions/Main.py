d10dc7cde493aa85a37bcf8f65d24275ee948da1
vikram <vikramdev772@gmail.com>
2023-11-15 19:19:51 +0530

    Functions

---
+++ functions/Main.py
@@ -0,0 +1,51 @@
+"""
+        Function
+
+function is a block of code which only runs when it is called.
+Function has a name for identification
+
+"""
+
+"""
+
+        keyword = def
+    def functionName (args):
+        code
+        code 
+    indentation
+
+
+"""
+
+n=int(input("\n\t Enter a number  : "))
+
+while n:
+    print(n,"\t MySirG")
+    n-=1
+print()
+
+#          Enter a number  : 5
+# 5        MySirG
+# 4        MySirG
+# 3        MySirG
+# 2        MySirG
+# 1        MySirG
+
+
+def f1():
+    n=int(input("\n\t Enter a number  : "))
+    while n:
+        print(n,"\t MySirG")
+        n=n-1
+print()
+
+f1()
+
+#  Enter a number  : 6
+# 6        MySirG
+# 5        MySirG
+# 4        MySirG
+# 3        MySirG
+# 2        MySirG
+# 1        MySirG
+
