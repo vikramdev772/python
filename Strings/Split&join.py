5c82ece2780938e836f2329dab283d9db744e656
vikram <vikramdev772@gmail.com>
2023-11-17 14:10:21 +0530

    split & join

---
+++ Strings/Split&join.py
@@ -0,0 +1,45 @@
+print("\n\t Split and join \n")
+
+s1="MERN JSF PF"
+# split=s1.Split(' ')
+
+basestring = " " #(space)
+s1.split(' ')
+print(s1)
+#   Split and join
+
+# MERN JSF PF
+# print(f"\n\t spliting the string : {split}")
+
+"""split function always return list of str type elements """
+
+""" split ==> str - list of str  base string
+    join  ==> listofstr-str base string """
+
+print("\n\t Enter numbers seprated by comas \n")
+s1.split(',')
+print(s1)
+print(s1[1:10:2])
+# ENJFP
+# basestring.join(list of str)
+
+# ''.join(l1)==> 'djanogo flask '
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
